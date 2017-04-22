c4e8e4ed18a434d18dffd4469e535511e2e15743
surekhad96 <surekhasugan96@gmail.com>
2017-04-22 15:36:54 +0530

    Create sumofnaturalno.c

---
+++ sumofnaturalno.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,s=0,i;
+printf("Enter the positive integers\n");
+scanf("%d",&n);
+for(i=0;i<=n;++i)
+{
+sum+=i;
+}
+printf("Sum= %d",sum);
+getch();
+return 0;
+}
